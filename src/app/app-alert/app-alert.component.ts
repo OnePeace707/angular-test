b748fce522848ca5c2e06b05bc74872498860e9b
OnePeace707 <1583766870@qq.com>
2019-11-01 17:40:59 +0800

    3 files modified, 1 added

--- src/app/app-alert/app-alert.component.ts
+++ src/app/app-alert/app-alert.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
+import { DogService } from '../dog.service';
 
 @Component({
   selector: 'app-app-alert',
@@ -12,9 +13,16 @@ export class AppAlertComponent implements OnInit {
   @Input() product;
   @Output() notify = new EventEmitter();
   
-  constructor() { }
+  constructor(
+    private dogService : DogService
+  ) { }
 
   ngOnInit() {
+  }
+
+  addDog(product){
+    window.alert("add a new dog");
+    this.dogService.addDog(product);
   }
 
 }
